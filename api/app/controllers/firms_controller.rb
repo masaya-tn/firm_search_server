912f1e9319435439ba0c9884cb5a5e6b2866b581
masaya-tn <uvermasayan@icloud.com>
2023-06-15 01:54:21 +0900

    修正

--- api/app/controllers/firms_controller.rb
+++ api/app/controllers/firms_controller.rb
@@ -10,8 +10,7 @@ class FirmsController < ApplicationController
     @sales_min = params[:salesMin]
     @profits_max = params[:profitsMax]
     @profits_min = params[:profitsMin]
-    @search_pattern = prarams[:searchPattern]
-
+    @search_pattern = params[:searchPattern]
     case @search_pattern
     when 'and' then
       firms = and_search()
@@ -28,26 +27,27 @@ class FirmsController < ApplicationController
     firms = Firm.includes(:performances)
                 .yield_self{|firms| @status.present? ? firms.where(status: @status.to_i) : firms}
                 .yield_self{|firms| @firm_name.present? ? firms.where(firm_name: @firm_name) : firms}
-                .yield_self{|firms| @address.present? ? firms.where(address: @address) : firms}
+                .yield_self{|firms| @address.present? ? firms.where("address LIKE ?", "%#{@address}%") : firms}
                 .yield_self{|firms| @sales_max.present? ? firms.where(performances: {sales: ..@sales_max.to_i, year: "2022"}) : firms}
                 .yield_self{|firms| @sales_min.present? ? firms.where(performances: {sales: @sales_min.to_i.., year: "2022"}) : firms}
                 .yield_self{|firms| @profits_max.present? ? firms.where(performances: {profits: ..@profits_max.to_i, year: "2022"}) : firms}
                 .yield_self{|firms| @profits_min.present? ? firms.where(performances: {profits: @profits_min.to_i.., year: "2022"}) : firms}
                 .all
-    firms
+    render json: { firms: firms }
   end
 
   def or_search()
+    p @profits_max
     firms = Firm.includes(:performances)
                 .none
-                .yield_self{|firms| @status.present? ? firms.or(Firm.where(status: @status.to_i)) : firms}
-                .yield_self{|firms| @firm_name.present? ? firms.or(Firm.where(firm_name: @firm_name)) : firms}
-                .yield_self{|firms| @address.present? ? firms.or(Firm.where(address: @address)) : firms}
+                .yield_self{|firms| @status.present? ? firms.or(Firm.includes(:performances).where(status: @status.to_i)) : firms}
+                .yield_self{|firms| @firm_name.present? ? firms.or(Firm.includes(:performances).where(firm_name: @firm_name)) : firms}
+                .yield_self{|firms| @address.present? ? firms.or(Firm.includes(:performances).where("address LIKE ?", "%#{@address}%")) : firms}
                 .yield_self{|firms| @sales_max.present? ? firms.or(Firm.includes(:performances).where(performances: {sales: ..@sales_max.to_i, year: "2022"})) : firms}
                 .yield_self{|firms| @sales_min.present? ? firms.or(Firm.includes(:performances).where(performances: {sales: @sales_min.to_i.., year: "2022"})) : firms}
-                .yield_self{|firms| @profit_max.present? ? firms.or(Firm.includes(:performances).where(performances: {profits: ..@profits_max.to_i, year: "2022"})) : firms}
+                .yield_self{|firms| @profits_max.present? ? firms.or(Firm.includes(:performances).where(performances: {profits: ..@profits_max.to_i, year: "2022"})) : firms}
                 .yield_self{|firms| @profits_min.present? ? firms.or(Firm.includes(:performances).where(performances: {profits: @profits_min.to_i.., year: "2022"})) : firms}
                 
-    firms
+      render json: { firms: firms }
   end
 end
